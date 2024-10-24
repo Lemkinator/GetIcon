1551247a7f4fb8eae5ebd5a18603071cb8e79731
Leonard <leo@leonard-lemke.com>
2024-10-24 23:11:15 +0200

    update design lib

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,3 @@
 rootProject.name = "Get Icon"
 include(":app")
-
-//To include the design `lib' to your project as a local module,
-//you may add the following lines to your project's settings.gradle
-//with the Path to the `lib` folder of your local clone of the design-lib
-//https://github.com/tribalfs/oneui-design-sample-app
-include(":lib")
-project(":lib").projectDir = File("../oneui-design-sample-app/lib")
  
