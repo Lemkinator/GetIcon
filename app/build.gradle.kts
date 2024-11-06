c43590b257a0ecaae1c399e685caec811acc3a52
Leonard <leo@leonard-lemke.com>
2024-11-07 00:03:54 +0100

    release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "de.lemke.geticon"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.1.3"
+        versionCode = 14
+        versionName = "1.1.4"
         resourceConfigurations += listOf("en", "de")
     }
 
