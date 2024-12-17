2ac39c75e43a06ef5e375763a67823a3596dcc18
Leonard <leo@leonard-lemke.com>
2024-12-17 17:36:51 +0100

    update libs + use OnboardingTipsItemView from design lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,10 +91,10 @@ dependencies {
     implementation("sesl.androidx.apppickerview:apppickerview:1.0.1+1.0.1-sesl6+rev3")
     //SESL6(OneUI 6) Material Components + Design Lib + Icons
     implementation("sesl.com.google.android.material:material:1.12.0+1.0.23-sesl6+rev2")
-    implementation("io.github.tribalfs:oneui-design:0.3.3+oneui6")
+    implementation("io.github.tribalfs:oneui-design:0.3.4+oneui6")
     implementation("io.github.oneuiproject:icons:1.1.0")
 
-    implementation("io.github.lemkinator:common-utils:0.3.8")
+    implementation("io.github.lemkinator:common-utils:0.4.4")
 
     implementation("com.airbnb.android:lottie:6.6.1")
     implementation("com.google.android.play:review-ktx:2.0.2")
