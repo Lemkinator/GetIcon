f94b2a4930cdb11a690aaf5e12d4000503b9e00f
Leonard <leo@leonard-lemke.com>
2025-02-16 18:33:13 +0100

    release update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "de.lemke.geticon"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.2.5"
+        versionCode = 24
+        versionName = "1.2.6"
     }
 
     @Suppress("UnstableApiUsage")
