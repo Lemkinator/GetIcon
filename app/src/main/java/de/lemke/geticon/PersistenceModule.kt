2156048f880a817818529a691c7a43cc0112f648
Leonard <leo@leonard-lemke.com>
2026-05-08 00:23:16 +0200

    refactor: remove unnecessary Application inheritance from PersistenceModule

--- app/src/main/java/de/lemke/geticon/PersistenceModule.kt
+++ app/src/main/java/de/lemke/geticon/PersistenceModule.kt
@@ -1,6 +1,5 @@
 package de.lemke.geticon
 
-import android.app.Application
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
@@ -14,8 +13,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object
-PersistenceModule : Application() {
+object PersistenceModule {
     private val Context.userSettingsStore: DataStore<Preferences> by preferencesDataStore(name = "userSettings")
 
     @Provides
